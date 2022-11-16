44b395633d62bf9d120bdeab29ac87170d470868
joeappleton18 <joe.appleton@solent.ac.uk>
2022-11-16 19:45:31 +0000

    updating the application

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 #! /usr/bin/env node
-console.log("Hello, World!");
+const { execSync } = require("child_process");
 
-const {execSync} = require("child_process");
+const botCommit = () => {
+  execSync("git add -A", { stdio: false });
+  execSync("git commit -m 'bot commit'", { stdio: false });
+};
 
 function init() {
-	
+  try {
+    const currentBranch = execSync("git rev-parse --abbrev-ref HEAD")
+      .toString()
+      .replace("\n", "");
+    const nodeModulesBranch = `${currentBranch}_with_node_modules`;
+
+    
+
+  } catch (error) {
+    console.error(`could not complete: ${error.toString()}`);
+  }
 }
+
+init();
